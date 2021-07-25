cabeced44a37c89258ce680c6a311690e29ffbf0
unizef <zefstribos@gmail.com>
2021-07-25 18:15:37 +0200

    stripped and installed json-server

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <div id="nav"></div>
+  <router-view />
 </template>
 
 <style>
+body {
+  background-color: #f2f2f2f2;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
 }
 
 #nav {
