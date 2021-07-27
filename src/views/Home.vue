7844ede39357ab0a5d9022825091b467426a7f34
unizef <zefstribos@gmail.com>
2021-07-27 17:35:18 +0200

    fetching data to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
     Home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <h3>{{ project.title }}</h3>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,5 +13,16 @@
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((resp) => resp.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err));
+  },
 };
 </script>
