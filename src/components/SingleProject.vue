6bfd6cdaed36c1db39f77e0d590cfd054f47957f
unizef <zefstribos@gmail.com>
2021-07-30 11:23:52 +0200

    fetch delete request to db.json

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3 @click="showDetails = !showDetails">
+        {{ project.title }}
+      </h3>
+      <div class="icons">
+        <span class="material-icons">
+          edit
+        </span>
+        <span @click="deleteProject" class="material-icons">
+          delete
+        </span>
+        <span class="material-icons">
+          done
+        </span>
+      </div>
+      <div v-if="showDetails" class="details">
+        <p>{{ project.details }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["project"],
+  data() {
+    return {
+      uri: "http://localhost:3000/projects/" + this.project.id,
+      showDetails: false,
+    };
+  },
+  methods: {
+    deleteProject() {
+      fetch(this.uri, { method: "DELETE" });
+    },
+  },
+};
+</script>
+
+<style>
+.project {
+  margin: 20px auto;
+  background: white;
+  padding: 10px 20px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
+  border-left: 4px solid #e90074;
+}
+
+h3 {
+  cursor: pointer;
+}
+
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.material-icons {
+  font-size: 24px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
+}
+
+.material-icons:hover {
+  color: #777;
+}
+</style>
